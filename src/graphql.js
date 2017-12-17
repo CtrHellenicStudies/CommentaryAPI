95ada994875b5183bfeaea368fbb85dbc3fec3c2
Luke Hollis <lukehollis@gmail.com>
2017-12-17 12:49:54 -0500

    stitch remote schema from chs textserver with local commentary schmea

--- src/graphql.js
+++ src/graphql.js
@@ -1,17 +1,34 @@
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { formatError } from 'apollo-errors';
 import { GraphQLSchema, execute, subscribe } from 'graphql';
+import {
+	makeRemoteExecutableSchema, mergeSchemas, introspectSchema
+} from 'graphql-tools';
+import { createApolloFetch } from 'apollo-fetch';
 import { maskErrors } from 'graphql-errors';
 import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 import jwt from 'jsonwebtoken';
 
+// authentication
 import { jwtAuthenticate } from './authentication';
+
+// root query and mutation
 import RootQuery from './graphql/queries/rootQuery';
 import RootMutation from './graphql/mutations/rootMutation';
 
-// models
-import User from './models/user';
+/**
+ * Create a remote schema for merging with local schema definition
+ * via example Schema stitching from the repo mentioned here:
+ * https://dev-blog.apollodata.com/graphql-schema-stitching-8af23354ac37
+ */
+const createRemoteSchema = async (uri: string) => {
+	const fetcher = createApolloFetch({uri});
+	return makeRemoteExecutableSchema({
+		schema: await introspectSchema(fetcher),
+		fetcher
+	});
+}
 
 /**
  * Root schema
@@ -22,9 +39,14 @@ const RootSchema = new GraphQLSchema({
 	mutation: RootMutation,
 });
 
-// mask error messages
-maskErrors(RootSchema);
+// mask error messages for production
+if (process.env.NODE_ENV === "production") {
+	maskErrors(RootSchema);
+}
 
+/**
+ * For each graphql request add user and project to context
+ */
 const getGraphQLContext = req => ({
 	user: req.user,
 	project: req.project,
@@ -34,9 +56,15 @@ const getGraphQLContext = req => ({
  * Set up the graphQL HTTP endpoint
  * @param  {Object} app 	express app instance
  */
-export default function setupGraphql(app) {
+const setupGraphQL = async (app) => {
+	const chsTextserverSchema = await createRemoteSchema('http://text.chs.orphe.us/graphql');
+
+	const schema = mergeSchemas({
+	  schemas: [RootSchema, chsTextserverSchema],
+	});
+
 	app.use('/graphql', jwtAuthenticate, graphqlExpress(req => ({
-		schema: RootSchema,
+		schema,
 		context: getGraphQLContext(req),
 		formatError,
 	})));
@@ -45,3 +73,5 @@ export default function setupGraphql(app) {
 		endpointURL: '/graphql',
 	}));
 }
+
+export default setupGraphQL;
