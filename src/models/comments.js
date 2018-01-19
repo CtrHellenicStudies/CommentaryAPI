0c396034031d50dfd4a8dd483dccd8e1746667f2
posejdon <abujak@itsg.com.pl>
2018-01-19 15:19:42 +0100

    fix comments

--- src/models/comments.js
+++ src/models/comments.js
@@ -204,82 +204,81 @@ const CommentsModel = new mongoose.Schema({
 });
 
 
-// const COMMENT_ID_LENGTH = 7;
+const COMMENT_ID_LENGTH = 7;
 
-// const _getCommentURN = (comment) => {
-// 	const work = Works.findOne({ slug: comment.work.slug });
-// 	const tenant = Tenants.findOne({_id: comment.tenantId});
-// 	const urnPrefixV1 = 'urn:cts:CHS.Commentary';
-// 	const urnPrefixV2 = `urn:cts:CHS:Commentaries. ${tenant.subdomain.toUpperCase()}`;
-// 	// Use work tlg if it exists, otherwise, search for subwork tlg number
-// 	// Failing either, just use creator
-// 	let urnTLG = work.tlgCreator;
-// 	if (work.tlg && work.tlg.length) {
-// 		urnTLG += `.${work.tlg}`;
-// 	} else {
-// 		work.subworks.forEach((subwork) => {
-// 			if (
-// 					subwork.n === comment.subwork.n
-// 				&& subwork.tlgNumber
-// 				&& subwork.tlgNumber.length
-// 			) {
-// 				urnTLG += `.${subwork.tlgNumber}`;
-// 			}
-// 		});
-// 	}
+const _getCommentURN = (comment) => {
+	const work = Works.findOne({ slug: comment.work.slug });
+	const tenant = Tenants.findOne({_id: comment.tenantId});
+	const urnPrefixV1 = 'urn:cts:CHS.Commentary';
+	const urnPrefixV2 = `urn:cts:CHS:Commentaries. ${tenant.subdomain.toUpperCase()}`;
+	// Use work tlg if it exists, otherwise, search for subwork tlg number
+	// Failing either, just use creator
+	let urnTLG = work.tlgCreator;
+	if (work.tlg && work.tlg.length) {
+		urnTLG += `.${work.tlg}`;
+	} else {
+		work.subworks.forEach((subwork) => {
+			if (
+					subwork.n === comment.subwork.n
+				&& subwork.tlgNumber
+				&& subwork.tlgNumber.length
+			) {
+				urnTLG += `.${subwork.tlgNumber}`;
+			}
+		});
+	}
 
-// 	//
-// 	urnTLG += '.chsCommentary';
-// 	const workTitle = comment.work.title.replace(' ', '');
+	//
+	urnTLG += '.chsCommentary';
+	const workTitle = comment.work.title.replace(' ', '');
 
-// 	let urnComment = `${workTitle}.${comment.subwork.title}.${comment.lineFrom}`;
+	let urnComment = `${workTitle}.${comment.subwork.title}.${comment.lineFrom}`;
 
-// 	if (typeof comment.lineTo !== 'undefined' && comment.lineFrom !== comment.lineTo) {
-// 		urnComment += `-${comment.subwork.title}.${comment.lineTo}`;
-// 	}
+	if (typeof comment.lineTo !== 'undefined' && comment.lineFrom !== comment.lineTo) {
+		urnComment += `-${comment.subwork.title}.${comment.lineTo}`;
+	}
 
-// 	const urnCommentId = `${comment._id.slice(-COMMENT_ID_LENGTH)}`;
-// 	return {
-// 		v1: `${urnPrefixV1}:${urnComment}.${urnCommentId}`,
-// 		v2: `${urnPrefixV2}:${urnComment}.${urnCommentId}`};
-// };
+	const urnCommentId = `${comment._id.slice(-COMMENT_ID_LENGTH)}`;
+	return {
+		v1: `${urnPrefixV1}:${urnComment}.${urnCommentId}`,
+		v2: `${urnPrefixV2}:${urnComment}.${urnCommentId}`};
+};
 
-// const _getAnnotationURN = (comment) => {
-// 	const book = Books.findOne({ 'chapters.url': comment.bookChapterUrl });
-// 	const chapter = _.find(book.chapters, c => c.url === comment.bookChapterUrl);
-// 	const tenant = Tenants.findOne({_id: comment.tenantId});
-// 	const urnPrefixV1 = 'urn:cts:CHS.Annotations';
-// 	const urnPrefixV2 = `urn:cts:CHS:Annotations. ${tenant.subdomain.toUpperCase()}`;
-// 	const urnBook = `${book.authorURN}.${book.slug}`;
-// 	const urnComment = `${chapter.n}.${comment.paragraphN}`;
-// 	const urnCommentId = `${comment._id.slice(-COMMENT_ID_LENGTH)}`;
+const _getAnnotationURN = (comment) => {
+	const book = Books.findOne({ 'chapters.url': comment.bookChapterUrl });
+	const chapter = _.find(book.chapters, c => c.url === comment.bookChapterUrl);
+	const tenant = Tenants.findOne({_id: comment.tenantId});
+	const urnPrefixV1 = 'urn:cts:CHS.Annotations';
+	const urnPrefixV2 = `urn:cts:CHS:Annotations. ${tenant.subdomain.toUpperCase()}`;
+	const urnBook = `${book.authorURN}.${book.slug}`;
+	const urnComment = `${chapter.n}.${comment.paragraphN}`;
+	const urnCommentId = `${comment._id.slice(-COMMENT_ID_LENGTH)}`;
 
-// 	return {
-// 		v1: `${urnPrefixV1}:${urnComment}.${urnCommentId}`,
-// 		v2: `${urnPrefixV2}:${urnComment}.${urnCommentId}`};
-// };
+	return {
+		v1: `${urnPrefixV1}:${urnComment}.${urnCommentId}`,
+		v2: `${urnPrefixV2}:${urnComment}.${urnCommentId}`};
+};
 
-// function getURN(comment) {
-// 	if (comment.isAnnotation) {
-// 		return _getAnnotationURN(comment);
-// 	}
+function getURN(comment) {
+	if (comment.isAnnotation) {
+		return _getAnnotationURN(comment);
+	}
 
-// 	return _getCommentURN(comment);
-// }
+	return _getCommentURN(comment);
+}
 
-// hooks:
-// CommentsModel.pre('insert', function(userId, doc) {
-// 	doc.urn = getURN(doc);
-// });
+CommentsModel.pre('insert', function(userId, doc) {
+	doc.urn = getURN(doc);
+});
 
-// CommentsModel.pre('update', function(userId, doc, fieldNames, modifier, options) {
-// 	if(modifier) {
-// 		modifier.$set.urn = getURN(doc);
-// 	}
-// });
+CommentsModel.pre('update', function(userId, doc, fieldNames, modifier, options) {
+	if (modifier) {
+		modifier.$set.urn = getURN(doc);
+	}
+});
 
 const Comments = mongoose.model('Comments', CommentsModel);
 
 export default Comments;
 
-// export { getURN };
+export { getURN };
