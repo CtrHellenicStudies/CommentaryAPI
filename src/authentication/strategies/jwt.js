430059194c81c91cff6f62e56d9b338b01dc4a4e
Luke Hollis <lukehollis@gmail.com>
2018-02-05 00:16:34 -0500

    fix lookup of user account by jwt in auth header without userLoader

--- src/authentication/strategies/jwt.js
+++ src/authentication/strategies/jwt.js
@@ -1,6 +1,5 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import DataLoader from 'dataloader';
 
 // models
 import User from '../../models/user';
@@ -13,10 +12,11 @@ const setupJWTStrategy = (app) => {
 		secretOrKey: process.env.JWT_SECRET,
 	};
 
+	// Could be made faster with Redis loader if need be
+	// http://gitlab.archimedes.digital/archimedes/orpheus-api/blob/master/src/authentication/strategies/jwt.js#L11
 	passport.use(new JwtStrategy(JWTOptions, async (jwtPayload, done) => {
-
 		try {
-			const user = await userLoader.load(jwtPayload._id);
+			const user = await User.findOne({ _id: jwtPayload._id });
 
 			if (user) {
 				done(null, user);
