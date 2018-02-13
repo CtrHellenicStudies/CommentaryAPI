4fb947b525b2057bb2b634e6c03980f129ad3d7d
Alan Mroczek <amroczek@itsg.com.pl>
2018-02-13 10:38:48 +0100

    add health check only for / route

--- src/health-check.js
+++ src/health-check.js
@@ -1,3 +1,11 @@
-const healthCheck = (req, res) => res.status(200).send('OK');
+const healthCheck = (req, res, next) => {
+
+	if (req.path === '/') {
+		res.status(200).send('OK');
+	}
+
+	next();
+
+};
 
 export default healthCheck;
